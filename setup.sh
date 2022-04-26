20b4945100d44435a13048b28d1ea08aa94c34b4
Michael Connor <michaelconnor468@gmail.com>
2022-04-25 20:47:59 -0400

    added ability to sync package list upon push

--- setup.sh
+++ setup.sh
@@ -2,21 +2,45 @@
 DOTFILES_REPO="git@github.com:michaelconnor468/Dotfiles.git"
 
 push () {
+    if [ $# -eq 2 -a $2 = "all" ]; then
+        pacman -Qq > pacman.txt
+    fi
     # use rsync to only update dirs that exist in both the repo and system
-    echo push
+    echo $*
 }
 
 pull () {
     # use rsync to simply get stuff from the repo
+    # do for config and etc in the various locations
     echo pull
 }
 
-if ! [ $# -eq 1 ]; then
-    echo "Usage: setup.sh <push|pull>"
+install () {
+    # install yay if not present
+    # install all packages from pacman.txt
+    # pull
+    echo install
+}
+
+ehco_usage () {
+    echo "Usage: setup.sh <push|pull|install>"
+}
+
+if [ $# -lt 1 ]; then
+    ehco_usage
     exit 1
 fi
+
 if [ $1 = "push" ]; then
-    push
-else
+    push $*
+    exit 0
+elif [ $1 = "pull" ]; then
     pull
+    exit 0
+elif [ $1 = "install" ]; then
+    install
+    exit 0
+else
+    ehco_usage
+    exit 1
 fi
