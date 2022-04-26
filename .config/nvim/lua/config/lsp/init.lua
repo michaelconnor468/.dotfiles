6b9590fa87f258f8e86cf1db460f6518e2b09271
Michael Connor <michaelconnor468@gmail.com>
2022-04-25 20:15:57 -0400

    remove old .config

--- .config/nvim/lua/config/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local ok, _ = pcall(require, 'lspconfig')
-if not ok then
-    print('Could not procure lspconfig')
-    return
-end
-
-require('config.lsp.lsp-installer')
-require('config.lsp.handlers').setup()
--- .config/zsh/hooks/linuxhook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [[ "$(uname -a | awk '{print $2}')" == "arch" ]]; then
-    source ~/.config/zsh/hooks/desktophook.sh
-fi
