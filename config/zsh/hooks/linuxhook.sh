bf5822f6cce8a886a44d048e4612ce4f8a2cf9f6
Michael James Connor <michaelconnor468@gmail.com>
2022-11-17 08:13:02 -0500

    enhance lscolors and add script to measure network statistics on linux

--- config/zsh/hooks/linuxhook.sh
+++ config/zsh/hooks/linuxhook.sh
@@ -2,3 +2,28 @@
 if [[ "$(uname -a | awk '{print $2}')" == "arch" ]]; then
     source ~/.config/zsh/hooks/desktophook.sh
 fi
+
+function enstat {
+    local prefix="/sys/class/net/enp4s0/statistics/"
+    if [[ "$#" -eq 2 || $1 = "bytes" || $1 = "packets" || $1 = "errors" || $1 = "dropped" || $2 = "in" || $2 = "out" ]]; then
+        if [[ $2 = "in" ]]; then
+            prefix=$prefix"rx_"$1
+        else
+            prefix=$prefix"tx_"$1
+        fi
+    else
+        echo "usage: enstat [bytes|packets|errors|dropped] [in|out]"
+        return 1
+    fi
+
+    local start="$(cat $prefix)"
+    printf "Measuring Delta... <Press Enter To Finish>\n"
+    read -r trash
+    local end="$(cat $prefix)"
+
+    if [ "$1" = "bytes" ]; then
+        printf "%4sB \n" $(numfmt --to=iec $(($end-$start)))
+    else
+        printf "%4s \n" $(($end-$start))
+    fi
+}
