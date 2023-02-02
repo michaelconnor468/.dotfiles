f2ddf23299c34eeed1b0404b30158196dc73d7aa
Michael James Connor <michaelconnor468@gmail.com>
2023-02-02 10:02:53 -0500

    default to current directory for jdtls root dir

--- config/nvim/ftplugin/java.lua
+++ config/nvim/ftplugin/java.lua
@@ -1,46 +1,49 @@
 local capabilities = require('config.lsp.handlers').capabilities
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local home = os.getenv "HOME"
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local home = os.getenv 'HOME'
 
-jdtls = require("jdtls")
+jdtls = require('jdtls')
 
-if vim.fn.has "mac" == 1 then
-    CONFIG = "mac"
-elseif vim.fn.has "unix" == 1 then
-    CONFIG = "linux"
+if vim.fn.has 'mac' == 1 then
+    CONFIG = 'mac'
+elseif vim.fn.has 'unix' == 1 then
+    CONFIG = 'linux'
 end
 
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = require("jdtls.setup").find_root(root_markers)
+local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+local root_dir = require('jdtls.setup').find_root(root_markers)
+if root_dir == '' then
+    root_dir = vim.fn.getcwd()
+end
 
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
 local config = {
     cmd = {
-        "java",
-        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        "-Dosgi.bundles.defaultStartLevel=4",
-        "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        "-Dlog.protocol=true",
-        "-Dlog.level=ALL",
-        "-Xms1g",
-        "--add-modules=ALL-SYSTEM",
-        "--add-opens",
-        "java.base/java.util=ALL-UNNAMED",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-        "-jar",
-        vim.fn.glob(home .. "/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-        "-configuration",
-        home .. "/.local/share/nvim/lsp_servers/jdtls/config_" .. CONFIG,
-        "-data",
-        home .. "/.local/share/nvim/lsp_servers/jdtls/data",
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xms1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+        '-jar',
+        vim.fn.glob(home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+        '-configuration',
+        home .. '/.local/share/nvim/lsp_servers/jdtls/config_' .. CONFIG,
+        '-data',
+        home .. '/.local/share/nvim/lsp_servers/jdtls/data',
     },
 
-    on_attach = require("config.lsp.handlers").on_attach,
+    on_attach = require('config.lsp.handlers').on_attach,
     capabilities = capabilities,
     root_dir = root_dir,
     settings = {
@@ -49,11 +52,11 @@ local config = {
                 downloadSources = true,
             },
             configuration = {
-                updateBuildConfiguration = "interactive",
+                updateBuildConfiguration = 'interactive',
                 runtimes = {
                     {
-                        name = "JavaSE-17",
-                        path = "/usr/lib/jvm/java-17-openjdk/",
+                        name = 'JavaSE-17',
+                        path = '/usr/lib/jvm/java-17-openjdk/',
                     },
                 }
             },
@@ -71,7 +74,7 @@ local config = {
             },
             inlayHints = {
                 parameterNames = {
-                    enabled = "all",
+                    enabled = 'all',
                 },
             },
             format = {
@@ -81,11 +84,11 @@ local config = {
         signatureHelp = { enabled = true },
         completion = {
             favoriteStaticMembers = {
-                "org.junit.jupiter.api.Assertions.*",
-                "org.mockito.Mockito.*",
+                'org.junit.jupiter.api.Assertions.*',
+                'org.mockito.Mockito.*',
             },
         },
-        contentProvider = { preferred = "fernflower" },
+        contentProvider = { preferred = 'fernflower' },
             extendedClientCapabilities = extendedClientCapabilities,
             sources = {
                 organizeImports = {
@@ -95,7 +98,7 @@ local config = {
         },
         codeGeneration = {
             toString = {
-                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+                template = '${object.className}{${member.name()}=${member.value}, ${otherMembers}}',
             },
             useBlocks = true,
         },
